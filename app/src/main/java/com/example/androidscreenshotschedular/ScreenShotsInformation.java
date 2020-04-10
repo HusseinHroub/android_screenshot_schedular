6877cbffc8fb2813c3c08fecb8efd17af2b969b5
hussein <husseinwhatever>
2020-04-10 11:07:53 +0300

    added StartActivit, and working on ScreenShotsInformation activity

---
+++ app/src/main/java/com/example/androidscreenshotschedular/ScreenShotsInformation.java
@@ -0,0 +1,13 @@
+package com.example.androidscreenshotschedular;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenShotsInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_shots_information);
+    }
+}
--- app/src/main/java/com/example/androidscreenshotschedular/StartActivity.java
+++ app/src/main/java/com/example/androidscreenshotschedular/StartActivity.java
@@ -1,8 +1,11 @@
 package com.example.androidscreenshotschedular;
 
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_screen);
+        initializeTimeUnitsDataInSpinner();
+    }
+
+    private void initializeTimeUnitsDataInSpinner() {
+        Spinner spinner = findViewById(R.id.time_unit_spinner);
+        ArrayAdapter<CharSequence> adapter = createAdapterWithDefaultLayout();
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    private ArrayAdapter<CharSequence> createAdapterWithDefaultLayout() {
+        return ArrayAdapter.createFromResource(this,
+                R.array.time_units, android.R.layout.simple_spinner_item);
+    }
+
+    public void startScreenShotScheduler(View startScreenShotSchedulerButton)
+    {
+
     }
 }
