1f8f756036ede3c101680dab3aa815d5190abd10
hussein <husseinwhatever>
2020-04-10 22:40:09 +0300

    Finished Image view activity, time to start with the actual API and C# server implemntation, will do this later, I will sleep, Bye

--- app/src/main/java/com/example/androidscreenshotschedular/activity/ImageViewActivity.java
+++ app/src/main/java/com/example/androidscreenshotschedular/activity/ImageViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidscreenshotschedular.activity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.androidscreenshotschedular.R;
 import com.example.androidscreenshotschedular.utils.BitMapReading;
@@ -13,12 +14,22 @@ public class ImageViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
-        initializePhotoView();
+        DisplayMetrics displayMetrics = getDisplayPhoneScreenMetrics();
+        setupPhotoView(displayMetrics.heightPixels, displayMetrics.widthPixels);
+
     }
 
-    private void initializePhotoView() {
+    private DisplayMetrics getDisplayPhoneScreenMetrics() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics;
+    }
+
+    private void setupPhotoView(int height, int width) {
         PhotoView photoView = findViewById(R.id.photo_view);
         String imagePath = getIntent().getStringExtra(Constants.INTENT_IMAGE_PATH_FOR_FULL_VIEW);
-        photoView.setImageBitmap(new BitMapReading(photoView.getWidth(), photoView.getHeight()).decodeImageFromLocation(imagePath));
+        photoView.setImageBitmap(new BitMapReading(height, width).decodeImageFromLocation(imagePath));
     }
+
+
 }
